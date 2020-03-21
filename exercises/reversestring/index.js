1b19839cfba3133e3bcae56e7176beeaf86c86ef
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-21 12:22:21 -0400

    Finish first exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return Array.from(str).reverse().join('');
+// }
+
+// function reverse(str) {
+//   let acc = '';
+//   for(let i = 0; i < str.length; i++) {
+//     acc = str[i] + acc
+//   }
+//   return acc;
+// }
+
+// function reverse(str) {
+//   let acc = '';
+//   for (let letter of str) {
+//     acc = letter + acc;
+//   }
+//   return acc;
+// }
+
+function reverse(str) {
+  return Array.from(str).reduce((acc, letter) => letter + acc, '');
+}
 
 module.exports = reverse;
