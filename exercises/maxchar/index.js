69548a8cc4e1378c90457b55c1585615746a47ac
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-22 10:20:20 -0400

    Compelte the maxchar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charCount = {};
+
+  for (let char of str) {
+    charCount[char] = charCount[char] + 1 || 1;
+  }
+
+  let maxChar = '';
+  for (let char in charCount) {
+    maxChar = maxChar || char;
+
+    if (charCount[char] > charCount[maxChar]) {
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
