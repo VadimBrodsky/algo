5ef2b3594a92884eb0737782a615a702711cdbb7
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-22 16:42:47 -0400

    WIP steps exercise

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,12 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log('#'.repeat(i));
+  }
+}
+
+steps(3)
 
 module.exports = steps;
