3b2cf7664250a89c5e40cd5890ffadbad2fa1715
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-22 16:07:29 -0400

    Solve the anagram exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   let nonWords = /[^\w]/g;
+
+//   function strToCharHash(string) {
+//     return Array.from(string).reduce((acc, c) => {
+//       acc[c] = acc + 1 || 0;
+//       return acc;
+//     }, [])
+//   }
+
+//   let hashA = strToCharHash(stringA.replace(nonWords, '').toLowerCase());
+//   let hashB = strToCharHash(stringB.replace(nonWords, '').toLowerCase());
+
+//   if (Object.keys(hashA).length !== Object.keys(hashB).length) {
+//     return false;
+//   }
+
+//   return Object.keys(hashA).every((char) => hashA[char] === hashB[char]);
+// }
+
+function anagrams(stringA, stringB) {
+  const sortString = (string) =>
+    string
+      .replace(/[^\w]/g, '')
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join();
+
+  return sortString(stringA) === sortString(stringB);
+}
 
 module.exports = anagrams;
