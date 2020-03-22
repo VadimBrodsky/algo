2dc90e7913a4a85e5f6b5aee2e6e19f570a2239c
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-22 16:23:34 -0400

    Complete the capitalize exercise

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,21 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//   return str
+//     .split(' ')
+//     .map((word) => word[0].toUpperCase() + word.slice(1))
+//     .join(' ');
+// }
+
+function capitalize(str) {
+  let captalizedStr = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    captalizedStr += str[i - 1] === ' ' ? str[i].toUpperCase() : str[i];
+  }
+
+  return captalizedStr;
+}
 
 module.exports = capitalize;
