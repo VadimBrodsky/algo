9070c9de7c2f86e1469c09856e79492c207d41ae
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-22 09:42:21 -0400

    Complete reverseint exercise

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//   let reversed = Number.parseInt(
+//     Array.from(Math.abs(n).toString())
+//       .reverse()
+//       .join(''),
+//   );
+
+//   return reversed * Math.sign(n);
+// }
+
+function reverseInt(n) {
+  let reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return Number.parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
