2a48f3278a44311fd8747f26316ebccd2612ff5f
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-22 21:29:54 -0400

    Complete the steps exercise

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,12 +17,44 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log('#'.repeat(i));
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log('#'.repeat(i).padEnd(n, ' '));
+//   }
+// }
+
+// function steps(n) {
+//   for (let i = 0; i < n; i++) {
+//     let step = '';
+
+//     for (let j = 0; j < n; j++) {
+//       step += j <= i ? '#' : ' ';
+//     }
+
+//     console.log(step);
+//   }
+// }
+
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
 }
 
-steps(3)
+steps(5)
 
 module.exports = steps;
