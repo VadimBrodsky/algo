96095c72e5ba6264450971e44ff09765835d4a0f
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-24 21:54:10 -0400

    Complete the pyramid exercise in iterative way

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//   let width = n + n - 1;
+
+//   for (let row = 1, hashes = 1; row <= n; row++) {
+//     let str = '';
+//     let spacesOnEachSide = (width - hashes) / 2;
+
+//     for (let col = 1; col <= width; col++) {
+//       if (col > spacesOnEachSide && col <= width - spacesOnEachSide) {
+//         str += '#';
+//       } else {
+//         str += ' ';
+//       }
+//     }
+
+//     console.log(str);
+//     hashes += 2;
+//   }
+// }
+
+function pyramid(n) {
+  let width = n + n - 1;
+  let center = Math.floor(width / 2);
+
+  for (let row = 0; row < n; row++) {
+    let str = '';
+
+    for (let col = 0; col < width; col++) {
+      if (center - row <= col && center + row >= col) {
+        str += '#';
+      } else {
+        str += ' ';
+      }
+    }
+
+    console.log(str);
+  }
+}
 
 module.exports = pyramid;
