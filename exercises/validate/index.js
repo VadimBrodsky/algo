4f189c0c0bc783dd42a74cd5a8958a63f0679c97
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-28 22:10:59 -0400

    Complete initial implementation of BST validation

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,31 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if (node === null) {
+    return true;
+  }
+
+  if (node.left) {
+    if (min === null) {
+      return validate(node, node.data, max);
+    } 
+      return validate(node.left, null, node.data);
+  }
+
+  if (node.right) {
+    if (max === null) {
+      return validate(node.right, node.data, node.data);
+    } else {
+      return validate(node.right, node.data, null);
+    }
+  }
+
+  if (node.data > max || node.data < min) {
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validate;
