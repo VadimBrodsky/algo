c8377436146619f83f1f83a3b268881816381341
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-28 20:32:31 -0400

    Complete the Tree Level Width exercise

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let nodes = [root, 'STOP'];
+  let counters = [0];
+
+  while (nodes.length > 1) {
+    let currentNode = nodes.shift();
+
+    if (currentNode === 'STOP') {
+      counters.push(0);
+      nodes.push(currentNode);
+    } else {
+      nodes.push(...currentNode.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
