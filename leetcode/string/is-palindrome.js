8f8cf764021e5a52d41ca2911df8e523ed05054e
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-04-04 14:03:11 -0400

    Add string questions

---
+++ leetcode/string/is-palindrome.js
@@ -0,0 +1,27 @@
+let { test } = require('../../_utils/test');
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/883/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  let cleanString = s.toLowerCase().match(/[a-zA-Z0-9]/g);
+
+  if (!cleanString) {
+    return true;
+  }
+
+  let reversed = [...cleanString].reverse();
+  return cleanString.join('') === reversed.join('');
+};
+
+test((assert) => {
+  assert.equal(isPalindrome('A man, a plan, a canal: Panama'), true);
+  assert.equal(isPalindrome('race a car'), false);
+  assert.equal(isPalindrome(''), true);
+  assert.equal(isPalindrome(' '), true);
+  assert.equal(isPalindrome('.'), true);
+  assert.equal(isPalindrome('0P'), false);
+  assert.equal(isPalindrome('a'), true);
+});
---
+++ leetcode/string/my-atoi.js
@@ -0,0 +1,30 @@
+let { test } = require('../../_utils/test');
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/884/
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+  let i = Number.parseInt(str.trimStart());
+  let max = Math.pow(2, 31);
+
+  if (!i) {
+    return 0;
+  } else if (Math.abs(i) >= max && Math.sign(i) === -1) {
+    return max * Math.sign(i);
+  } else if (i >= max) {
+    return max - 1;
+  }
+
+  return i;
+};
+
+test((assert) => {
+  assert.equal(myAtoi('42'), 42);
+  assert.equal(myAtoi('   -42'), -42);
+  assert.equal(myAtoi('4193 with words'), 4193);
+  assert.equal(myAtoi('words and 987'), 0);
+  assert.equal(myAtoi('-91283472332'), -2147483648);
+  assert.equal(myAtoi('2147483648'), 2147483647);
+});
