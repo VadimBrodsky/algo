d380ad5d8ba642eb953a3cd0b24054228b1efea5
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-04-09 20:25:40 -0400

    Improve the test tooling

--- _utils/test.js
+++ _utils/test.js
@@ -1,8 +1,23 @@
 let assert = require('assert');
 
+let noop = () => {};
+
 let test = (cb) => cb(assert);
-test.skip = (cb) => {};
+test.skip = noop;
+
+let it = (name, cb) => {
+  console.log(`TEST: ${name}`);
+  try {
+    cb(assert);
+    console.log('PASSED!');
+  } catch (e) {
+    console.log('FAIL:');
+    console.log(e);
+  }
+};
+it.skip = noop;
 
 module.exports = {
   test,
+  it,
 };
