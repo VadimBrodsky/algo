51842ac36854fbeadc76f63539653f8fd9370370
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-04-12 19:30:38 -0400

    Complete the insertion sort exercise

--- algoexpert/insertion-sort.js
+++ algoexpert/insertion-sort.js
@@ -1,6 +1,26 @@
 const { it } = require('../_utils/test.js');
 
-let insertionSort = (array) => {};
+// Time O(N^2)
+// Space O(1)
+let insertionSort = (array) => {
+  let swap = (array, indexA, indexB) => {
+    let temp = array[indexA];
+    array[indexA] = array[indexB];
+    array[indexB] = temp;
+  };
+
+  for (let i = 1; i < array.length; i++) {
+    let current = i;
+    let prev = i - 1;
+
+    while (array[current] < array[prev] && prev >= 0) {
+      swap(array, current, prev);
+      current--;
+      prev--;
+    }
+  }
+  return array;
+};
 
 it('should sort an array of numbers', (assert) => {
   let array = [8, 5, 2, 9, 5, 6, 3];
