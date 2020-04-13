d9731fae36ff4a75e8ae573e69c6f833ee49ce44
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-04-12 20:58:08 -0400

    WIP Bubble Sort exercise

---
+++ algoexpert/bubble-sort.js
@@ -0,0 +1,24 @@
+const { it } = require('../_utils/test.js');
+
+let bubbleSort = (array) => {
+  let swap = (array, idxA, idxB) => {
+    let temp = array[idxA];
+    array[idxA] = array[idxB];
+    array[idxB] = temp;
+  };
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
+      }
+    }
+  }
+
+  return array;
+};
+
+it('should sort an array of numbers', (assert) => {
+  let array = [8, 5, 2, 9, 5, 6, 3];
+  assert.deepEqual(bubbleSort(array), [2, 3, 5, 5, 6, 8, 9]);
+});
