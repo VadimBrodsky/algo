5c916cfb584c4d122b88fdef8337db089e8228e3
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-04-18 13:34:42 -0400

    Complete the smallest difference exercise

--- algoexpert/smallest-difference.js
+++ algoexpert/smallest-difference.js
@@ -1,6 +1,39 @@
 let { it } = require('../_utils/test.js');
 
-let smallestDifference = (array1, array2) => {};
+// Time O(nLog(n) + mlog(m)) because of sorting
+// Space O(1)
+let smallestDifference = (arrayA, arrayB) => {
+  arrayA = arrayA.sort((a, b) => a - b);
+  arrayB = arrayB.sort((a, b) => a - b);
+
+  let minDifference = Infinity;
+  let smallestPair = [];
+
+  let pointerA = 0;
+  let pointerB = 0;
+
+  while (pointerA < arrayA.length && pointerB < arrayB.length) {
+    let numA = arrayA[pointerA];
+    let numB = arrayB[pointerB];
+    let difference = Math.abs(numA - numB);
+
+    if (minDifference > difference) {
+      minDifference = difference;
+      smallestPair = [numA, numB];
+    }
+
+    if (numA > numB) {
+      pointerB++;
+    } else if (numA < numB) {
+      pointerA++;
+    } else {
+      // when numbers are equal the difference is 0
+      return [numA, numB];
+    }
+  }
+
+  return smallestPair;
+};
 
 it('should return the 2 numbers that produce the smallest difference', (assert) => {
   let arrayA = [-1, 5, 10, 20, 28, 3];
