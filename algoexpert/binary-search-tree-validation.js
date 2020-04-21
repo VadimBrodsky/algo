5292b89e4f189823ed6f261a564238039672e34e
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-04-21 19:36:29 -0400

    Complete the BST validation exercise

--- algoexpert/binary-search-tree-validation.js
+++ algoexpert/binary-search-tree-validation.js
@@ -8,11 +8,30 @@ class Node {
   }
 }
 
+// Time O(n)
+// Space O(d), where d is the depth of the tree
 let validateBST = (tree) => {
+  let recurse = (node, min, max) => {
+    if (!node) {
+      return true;
+    }
 
-}
+    if (node.data < min || node.data > max) {
+      return false;
+    }
 
-it('should return true if the binary tree is a BST', () => {
+    return (
+      recurse(node.left, min, node.data) && recurse(node.right, node.data, max)
+    );
+  };
+
+  return (
+    recurse(tree.left, -Infinity, tree.data) &&
+    recurse(tree.right, tree.data, Infinity)
+  );
+};
+
+it('should return true if the binary tree is a BST', (assert) => {
   let tree = new Node(10);
   tree.left = new Node(5);
   tree.left.left = new Node(2);
@@ -23,11 +42,19 @@ it('should return true if the binary tree is a BST', () => {
   tree.right.left.right = new Node(14);
   tree.right.right = new Node(22);
 
-  expect.equal(validateBST(tree), true);
-})
+  assert.equal(validateBST(tree), true);
+});
 
-it('should return false if the binary tree is not a BST', () => {
+it('should return false if the binary tree is not a BST', (assert) => {
   let tree = new Node(10);
+  tree.left = new Node(5);
+  tree.left.left = new Node(2);
+  tree.left.right = new Node(5);
+  tree.left.left.left = new Node(1);
+  tree.right = new Node(15);
+  tree.right.left = new Node(16);
+  tree.right.left.right = new Node(14);
+  tree.right.right = new Node(22);
 
-  expect.equal(validateBST(tree), false);
-})
+  assert.equal(validateBST(tree), false);
+});
